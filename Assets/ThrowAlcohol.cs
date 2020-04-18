f3e106565315ebf6e9fa12ee68a56d3c3a24c57c
BadRAM <dgamebillda@gmail.com>
2020-04-17 21:26:51 -0700

    added game

---
+++ Assets/ThrowAlcohol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ThrowAlcohol : MonoBehaviour
+{
+    [SerializeField] private Transform projectileSpawn;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float speedMultiplierx;
+    [SerializeField] private float speedMultipliery;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject p = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+            Rigidbody r = p.GetComponent<Rigidbody>();
+            r.AddForce(projectileSpawn.right * Input.GetAxis("Mouse X") * speedMultiplierx, ForceMode.VelocityChange);
+            r.AddForce(projectileSpawn.forward * Input.GetAxis("Mouse Y") * speedMultipliery, ForceMode.VelocityChange);
+        }
+    }
+}
